33c57a3d4c7ae112ce373cf3d2d59c79cdc5ad3c
Rel1is <rellonchek01@gmail.com>
2025-11-19 15:45:11 +0300

    Initial Commit

---
+++ Метро Москвы/____________App.swift
@@ -0,0 +1,17 @@
+//
+//  ____________App.swift
+//  Метро Москвы
+//
+//  Created by Артем Терзиян on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ____________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
