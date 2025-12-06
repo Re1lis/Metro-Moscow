111a0e77e4f3839a90279fc5b848646c843b5b39
Re1lis <rellonchek01@gmail.com>
2025-12-06 18:10:49 +0300

    Сделан дизайн для экрана станции, добален некий функционал, что цвет при нажатии на кнопку меняется

--- Метро Москвы/ComponentsForViews/MainViewComponents/ListStationsOnBranch.swift
+++ Метро Москвы/ComponentsForViews/MainViewComponents/ListStationsOnBranch.swift
@@ -4,30 +4,35 @@ struct ListStationsOnBranch: View {
     @State var isSelectedStation: Station? = nil
     let branch: MetroStruct
     var body: some View {
-        VStack {
-            ForEach(branch.stations) { station in
-                Button {
-                    withAnimation(.easeIn(duration: 2)){
-                        self.isSelectedStation = station
-                    }
-                } label : {
-                    Text(station.name)
-                        .foregroundColor(.black)
-                        .font(.custom("moscowsansregular", size: 18))
-                        .overlay{
-                            RoundedRectangle(cornerRadius: 20)
-                                .stroke(Color.black, lineWidth: 1)
+        ScrollView {
+            VStack (spacing: 10) {
+                ForEach(branch.stations) { station in
+                    Button {
+                        withAnimation(.easeIn(duration: 2)){
+                            self.isSelectedStation = station
                         }
+                    } label : {
+                        Text(station.name)
+                            .frame(maxWidth: .infinity, maxHeight: 60)
+                            .foregroundColor(.black)
+                            .font(.custom("moscowsansregular", size: 23))
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.black, lineWidth: 1)
+                            }
+                    }
+                    .frame(width: .infinity, height: 50)
                 }
+                
             }
-            
+            .padding(.leading)
+            .padding(.trailing)
+            .sheet(item: $isSelectedStation) { station in
+                StationCard(station: station, branch: branch)
+            }
+            .navigationTitle(branch.name)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .ignoresSafeArea()
-        .sheet(item: $isSelectedStation) { station in
-            StationCard(station: station, branch: branch)
-        }
-        .navigationTitle(branch.name)
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
--- Метро Москвы/ComponentsForViews/ViewsForStation/StationCard.swift
+++ Метро Москвы/ComponentsForViews/ViewsForStation/StationCard.swift
@@ -7,21 +7,54 @@ struct StationCard: View {
     @EnvironmentObject var counterStation: counterIsVisitedStations
 
     var body: some View {
-        Text("Hello, World!")
-        if !station.isVisited {
-            Button("Посетить"){
-                withAnimation(.easeIn(duration: 1)) {
-                    counterStation.addOne()
-                    station.isVisited.toggle()
-                }
+        VStack{
+            Button{
+                dismiss()
+            } label : {
+                Text(station.name)
+                    .foregroundColor(.black)
+                    .font(.custom("moscowsansregular", size: 22))
             }
-        } else {
-            Button("Убрать посещение"){
-                withAnimation(.easeIn(duration: 1)) {
+            Text(station.isVisited ? "Станция посещена" : "Станция не посещена")
+                .font(Font.custom("moscowsansregular", size: 15))
+                .foregroundColor(.gray)
+        }
+        .padding(.top)
+        .frame(width: .infinity, alignment: .center)
+        Spacer()
+        Text("Информация")
+            .font(.custom("Kabel-Black", size: 25))
+            .frame(width: .infinity, alignment: .center)
+        
+        VStack {
+            Text("Год открытия станции: \(station.isOpeningYear)")
+            if let depth = station.depthStation {
+                Text("Глубина станции: \(depth) метров")
+            } else {
+                Text("Глубина станции: не указана")
+            }
+        }
+        .padding()
+        .font(.custom("moscowsansregular", size: 20))
+        .background(Color.gray)
+        .cornerRadius(20)
+        Spacer()
+        Button{
+            withAnimation(.easeIn(duration: 0.7)){
+                self.station.isVisited.toggle()
+                if !station.isVisited {
                     counterStation.takeAwayOne()
-                    station.isVisited.toggle()
+                } else {
+                    counterStation.addOne()
                 }
             }
+        } label : {
+            Text(station.isVisited ? "Убрать посещение" : "Посетить станцию")
+                .padding()
+                .foregroundColor(.black)
+                .font(.custom("moscowsansregular", size: 25))
+                .background(station.isVisited ? Color.green : Color.red)
+                .cornerRadius(20)
         }
     }
 }
